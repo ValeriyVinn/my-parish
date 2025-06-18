cf58bca63c6c39214c8d2cf9cffcd17212cbed76
ValeriyVinn <valeriy.all@gmail.com>
2025-06-18 20:51:12 +0300

    Added News section

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,134 +1,187 @@
+// import { useState } from "react";
+// import newsData from "../../data/news.json";
+// import css from "./News.module.css";
+
+// export default function News() {
+//   const [visibleCount, setVisibleCount] = useState(5);
+//   const visibleNews = newsData.slice(0, visibleCount);
+
+//   const handleLoadMore = () => {
+//     setVisibleCount((prev) => prev + 5);
+//   };
+
+//   return (
+//     <section className={css.section}>
+//       <h1 className={css.title}>Новини</h1>
+//       {visibleNews.map((item) => (
+//         <article key={item.id} className={css.card}>
+//           <div className={css.videoBlock}>
+//             <h2 className={css.heading}>{item.title}</h2>
+//             <div className={css.videoWrapper}>
+//               <iframe
+//                 src={`https://www.youtube.com/embed/${item.youtubeId}`}
+//                 title={item.title}
+//                 frameBorder="0"
+//                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+//                 allowFullScreen
+//               ></iframe>
+//             </div>
+//           </div>
+//           <p className={css.description}>{item.description}</p>
+//         </article>
+//       ))}
+
+//       {visibleCount < newsData.length && (
+//         <button onClick={handleLoadMore} className={css.loadMore}>
+//           Завантажити ще
+//         </button>
+//       )}
+//     </section>
+//   );
+// }
+
+// import { useState } from "react";
+// import newsData from "../../data/news.json";
+// import css from "./News.module.css";
+
+// export default function News() {
+//   const [visibleCount, setVisibleCount] = useState(5);
+//   const [expanded, setExpanded] = useState({}); // обʼєкт для кожної статті
+
+//   const visibleNews = newsData.slice(0, visibleCount);
+
+//   const handleLoadMore = () => {
+//     setVisibleCount((prev) => prev + 5);
+//   };
+
+//   const toggleReadMore = (id) => {
+//     setExpanded((prev) => ({
+//       ...prev,
+//       [id]: !prev[id],
+//     }));
+//   };
+
+//   return (
+//     <section className={css.section}>
+//       <div></div>
+//       <h1 className={css.title}>Новини</h1>
+//       {visibleNews.map((item) => (
+//         <article key={item.id} className={css.card}>
+//           <div className={css.videoBlock}>
+//             <h2 className={css.heading}>{item.title}</h2>
+//             <div className={css.videoWrapper}>
+//               <iframe
+//                 src={`https://www.youtube.com/embed/${item.youtubeId}`}
+//                 title={item.title}
+//                 frameBorder="0"
+//                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+//                 allowFullScreen
+//               ></iframe>
+//             </div>
+//           </div>
+
+
+          
+
+//           <div
+//             className={`${css.description} ${
+//               expanded[item.id] ? css.expanded : ""
+//             }`}
+//           >
+//             {item.description}
+//           </div>
+
+//           {item.description.length > 200 && (
+//             <button
+//               onClick={() => toggleReadMore(item.id)}
+//               className={css.readMoreBtn}
+//             >
+//               {expanded[item.id] ? "Згорнути" : "Читати далі..."}
+//             </button>
+//           )}
+//         </article>
+//       ))}
+
+//       {visibleCount < newsData.length && (
+//         <button onClick={handleLoadMore} className={css.loadMore}>
+//           Завантажити ще
+//         </button>
+//       )}
+//     </section>
+//   );
+// }
+
+
+import { useState } from "react";
+import newsData from "../../data/news.json";
 import css from "./News.module.css";
 
 export default function News() {
-  return (
-    <div className="container">
-      <h1 className={css.news}>Новини</h1>
+  const [visibleCount, setVisibleCount] = useState(5);
+  const [expanded, setExpanded] = useState({});
 
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita,
-        delectus incidunt? Adipisci, placeat ipsum. Numquam, autem quas
-        reiciendis recusandae dicta cumque ullam, quia distinctio deleniti earum
-        harum! Reprehenderit, neque inventore! Possimus, ducimus tempore saepe
-        quibusdam animi laudantium fuga non molestiae veritatis beatae sequi
-        quam eaque voluptate nemo, eveniet adipisci nostrum aspernatur, at
-        nulla? Quidem, in voluptatibus corrupti explicabo placeat repellendus.
-        Animi, sunt itaque laboriosam ad molestiae, quis explicabo aut excepturi
-        dolor similique sed suscipit iste accusamus aliquid repellendus dicta
-        minus? Rem accusamus quasi quis similique nam optio accusantium
-        expedita? Nihil!
-      </p>
-      <h2>Article One</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum hic
-        eligendi aperiam harum optio, quisquam consectetur dolorum possimus eos
-        ducimus eum recusandae veritatis. Odio quos perspiciatis est, reiciendis
-        ad dicta! Magnam inventore laudantium architecto ab provident magni
-        facere perspiciatis odio quisquam doloremque, quo nobis voluptatum
-        suscipit impedit eveniet itaque maiores? Optio similique magni hic illo
-        adipisci voluptatum reiciendis sunt consequuntur! At similique sint
-        harum! Illum fugiat maiores vel doloribus dicta commodi quisquam
-        incidunt fuga rerum inventore sit ad, eius blanditiis voluptatum? Id
-        sapiente cupiditate iste ipsam explicabo vitae adipisci fuga. Ullam vel
-        asperiores recusandae qui placeat, possimus voluptate neque laborum
-        debitis iure velit fugiat tenetur fugit labore, reiciendis quibusdam
-        corrupti nemo, error blanditiis ipsam incidunt. Odit error earum
-        molestiae itaque? Odio veniam est exercitationem expedita harum eveniet!
-        Iure, mollitia totam eveniet id deserunt nam eligendi nemo iste ab
-        dolores impedit! Saepe odio deserunt nobis officia molestiae soluta
-        veniam perspiciatis cum.
-      </p>
-      <h2>Article Two</h2>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae
-        itaque minus tempora nesciunt, deleniti voluptates, voluptatibus
-        cupiditate blanditiis ad officiis repellendus inventore debitis officia
-        asperiores. A rem harum nihil reiciendis? Vero nisi perspiciatis
-        laboriosam soluta corporis ratione veritatis unde, ab ipsam perferendis
-        illo expedita laborum optio molestiae distinctio, facilis repellendus
-        saepe commodi est aut. Nihil ratione odit repellat velit maiores? Illo
-        quod, consequuntur placeat tempore vitae, numquam quisquam ipsam, quam
-        maiores fugit provident veritatis nobis corrupti! Tempore pariatur
-        aliquam iure! Harum, nam. Ullam, libero dolorem excepturi maxime in
-        eveniet quia. Itaque modi fuga repudiandae asperiores voluptates
-        perferendis, repellendus voluptatibus obcaecati adipisci perspiciatis
-        cum eius, quidem consequatur qui facilis earum beatae, eveniet magni.
-        Rem eum amet eos ex! Maxime, laudantium molestias? Molestiae quod
-        adipisci aperiam, explicabo cum maiores magnam odit veritatis!
-        Laudantium, facilis mollitia quam expedita magnam dolorum nostrum
-        quibusdam dolores a, cumque non natus atque amet? Dolorum quam laborum
-        temporibus. Quaerat dolor, libero suscipit rerum excepturi soluta aut
-        sed alias numquam in aliquam saepe laboriosam omnis impedit corporis id!
-        Soluta, dolore eaque sunt fugit repudiandae tenetur est laudantium
-        voluptatum dolor? Iure molestias quidem reprehenderit! Odit facilis
-        impedit debitis ad temporibus quibusdam atque perferendis quas corporis,
-        delectus excepturi laudantium similique ullam repellat, asperiores unde
-        laboriosam, quasi accusamus consequatur magni mollitia consequuntur!
-      </p>
-      <h2>Article Three</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem earum
-        ut quos enim provident, accusamus voluptates quae labore ex suscipit?
-        Enim molestiae laboriosam nobis ipsa iusto voluptate quam deserunt
-        culpa. Deleniti dignissimos ad totam atque, adipisci iusto omnis
-        recusandae, nisi impedit, quae doloribus natus eveniet itaque neque in
-        pariatur eligendi dolor. Exercitationem ex aspernatur amet vitae
-        reprehenderit vero ad officiis. Perferendis veniam accusamus voluptates
-        illum impedit, magnam quis ipsa illo voluptatum voluptatibus, eligendi
-        quibusdam, sit suscipit vitae itaque aperiam aliquid fugit laudantium in
-        ea quod quasi consectetur saepe laboriosam. Minus? Porro adipisci
-        voluptatibus debitis ex. Architecto exercitationem impedit excepturi,
-        asperiores perspiciatis necessitatibus repudiandae repellat dignissimos,
-        saepe reiciendis cupiditate placeat aliquam. Corrupti iste id sequi est
-        quos. Voluptatibus voluptatem qui commodi? In nobis tenetur dicta
-        provident dolor! Quod expedita animi natus? Ipsum asperiores quidem
-        recusandae qui veniam distinctio sint nisi, odit, repellat assumenda
-        tempore officiis architecto delectus enim? Provident, consequuntur
-        deleniti? Iste, quae atque, eius eum fuga eligendi nobis expedita
-        molestias dolorem nostrum perferendis voluptatibus! Est illo ratione
-        corporis explicabo quae vel quas necessitatibus, impedit, sed eum
-        mollitia autem culpa magnam. Sapiente quidem voluptas maxime debitis
-        impedit rem aut odit eligendi reiciendis quibusdam. Nobis, obcaecati
-        impedit illum voluptatem praesentium dolores exercitationem soluta
-        quaerat placeat, commodi reprehenderit eius odit quasi, doloremque
-        voluptate? Temporibus, explicabo? Minima veniam ipsa deleniti dolores
-        doloremque aliquid nam dolorum. Deleniti maiores illo fugit aut
-        veritatis saepe, fuga itaque sit tenetur reprehenderit? Quo commodi id
-        aut accusamus repudiandae alias!
-      </p>
-      <h2>Article Four</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non tempore
-        qui, repudiandae doloremque quis laboriosam saepe quas eum suscipit nisi
-        est eos dolorem ducimus culpa quo eveniet, quisquam harum! Nemo? Nihil,
-        repudiandae, reiciendis optio blanditiis quisquam, labore perspiciatis
-        magni veniam perferendis similique reprehenderit. Necessitatibus,
-        exercitationem beatae sed corrupti maiores dolores in voluptates. Maxime
-        accusantium rem illo nesciunt sapiente quis laborum. Ipsa rerum nisi
-        laudantium nam non ut! Culpa eius temporibus in a sit, cupiditate odio
-        id ut, accusamus cum eaque, nobis ipsa. Voluptatibus voluptatem repellat
-        omnis dolor ea ullam qui. Quas aliquid accusantium natus voluptatibus
-        error cupiditate provident, at quod sunt, nemo quisquam expedita debitis
-        exercitationem, minima in odio similique a alias maiores dolorum
-        reprehenderit repudiandae perferendis voluptate. Ullam, sint. Harum
-        delectus unde officia necessitatibus alias, officiis provident dolore
-        assumenda in aliquam et inventore quis rerum iusto quam. Expedita enim
-        iure quas veritatis, quidem est dignissimos consectetur distinctio
-        facere fuga. Minus suscipit cum, cupiditate, distinctio esse vel ad odio
-        dolores reiciendis quod nemo facilis, atque vitae. Dolorem omnis
-        deserunt culpa quam officia, assumenda iure modi dicta cumque hic ipsum
-        qui. Cupiditate odio, molestias optio perferendis deleniti, voluptatem
-        voluptates amet necessitatibus illo obcaecati fugiat. Impedit aliquam
-        quam itaque, accusamus quas voluptatem voluptas quaerat, fugit
-        laudantium maiores magni delectus? Quidem, delectus ipsam? Sit officia
-        beatae placeat ex reprehenderit, cupiditate eum, rerum fuga laudantium
-        mollitia unde quo omnis pariatur amet quidem aspernatur. Ut, unde.
-        Repudiandae eius quisquam, expedita eligendi deserunt amet consequuntur
-        reiciendis. Maxime assumenda ab, blanditiis odit accusamus cupiditate?
-        Repellendus dolorum minima nisi voluptates, quisquam dignissimos
-        laborum. Modi nulla quae, mollitia, officiis voluptatem quidem vero
-        nostrum voluptate velit dicta magni unde fuga!
-      </p>
-    </div>
+  const visibleNews = newsData.slice(0, visibleCount);
+
+  const handleLoadMore = () => {
+    setVisibleCount((prev) => prev + 5);
+  };
+
+  const toggleReadMore = (id) => {
+    setExpanded((prev) => ({
+      ...prev,
+      [id]: !prev[id],
+    }));
+  };
+
+  const truncate = (text, maxLength) => {
+    if (text.length <= maxLength) return text;
+    return text.slice(0, maxLength).trim() + "… ";
+  };
+
+  return (
+    <section className={css.section}>
+      <h1 className={css.title}>Новини</h1>
+      {visibleNews.map((item) => {
+        const isExpanded = expanded[item.id];
+        const shouldTruncate = item.description.length > 300;
+
+        return (
+          <article key={item.id} className={css.card}>
+            <div className={css.videoBlock}>
+              <h2 className={css.heading}>{item.title}</h2>
+              <div className={css.videoWrapper}>
+                <iframe
+                  src={`https://www.youtube.com/embed/${item.youtubeId}`}
+                  title={item.title}
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                ></iframe>
+              </div>
+            </div>
+
+            <p className={css.description}>
+              {isExpanded || !shouldTruncate
+                ? item.description
+                : truncate(item.description, 300)}
+              {shouldTruncate && (
+                <button
+                  onClick={() => toggleReadMore(item.id)}
+                  className={css.inlineButton}
+                >
+                  {isExpanded ? " Згорнути" : " Читати далі..."}
+                </button>
+              )}
+            </p>
+          </article>
+        );
+      })}
+
+      {visibleCount < newsData.length && (
+        <button onClick={handleLoadMore} className={css.loadMore}>
+          Завантажити ще
+        </button>
+      )}
+    </section>
   );
 }
