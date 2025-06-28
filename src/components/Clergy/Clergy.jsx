5de125366b5590219f2045b676421a64b062a5d2
ValeriyVinn <valeriy.all@gmail.com>
2025-06-28 21:00:24 +0300

    upd: added priests

--- src/components/Clergy/Clergy.jsx
+++ src/components/Clergy/Clergy.jsx
@@ -1,7 +1,34 @@
+import css from "./Clergy.module.css";
+
 const Clergy = () => {
   return (
     <div className="container">
-      <h1>Настоятель храму - протиіерей Отець Олександр</h1>;
+      <table className={css.clergyTable}>
+        <tbody>
+          <tr>
+            <td>Настоятель</td>
+            <td className={css.clergyDignity}>Протиієрей </td>
+            <td className={css.clergyName}>Олександр Токарчук</td>
+          </tr>
+          <tr>
+            <td></td>
+            <td className={css.clergyDignity}>Ієрей </td>
+            <td className={css.clergyName}>Дмитро Турабов</td>
+          </tr>
+          <tr>
+            <td></td>
+
+            <td className={css.clergyDignity}>Диякон</td>
+            <td className={css.clergyName}>Олег Кучерук</td>
+          </tr>
+          <tr>
+            <td></td>
+            <td className={css.clergyDignity}>Диякон</td>
+            <td className={css.clergyName}>Андрій Прокопенко</td>
+          </tr>
+        </tbody>
+      </table>
+
     </div>
   );
 };
