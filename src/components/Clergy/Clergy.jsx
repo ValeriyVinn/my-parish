1a022d21d3fe8eb8f2fd3097728f7e32cf3b0385
Valeriy Svystun <valeriy.all@gmail.com>
2025-08-19 18:11:09 +0300

    added: prior phone

--- src/components/Clergy/Clergy.jsx
+++ src/components/Clergy/Clergy.jsx
@@ -4,14 +4,24 @@ const Clergy = () => {
   return (
     <div className="container">
       <article className={css.articleClergy}>
-
-
         <ul className={css.clergyList}>
-          <li>Настоятель</li>
-          <li className={css.clergyName}>Протоієрей Олександр Токарчук</li>
-          <li className={css.clergyName}>Ієрей Дмитро Турабов</li>
-          <li className={css.clergyName}>Диякон Олег Кучерук</li>
-          <li className={css.clergyName}>Диякон Андрій Прокопенко</li>
+          <h2 className={css.prior}>Настоятель</h2>
+          <ul className={css.clergyItem}>
+            <li className={css.clergyName}>Протоієрей Олександр Токарчук</li>
+            <li className={css.clergyPhone}>097 409 9913</li>
+          </ul>
+          <ul className={css.clergyItem}>
+            <li className={css.clergyName}>Ієрей Дмитро Турабов</li>
+            <li className={css.clergyPhone}></li>
+          </ul>
+          <ul className={css.clergyItem}>
+            <li className={css.clergyName}>Диякон Олег Кучерук</li>
+            <li className={css.clergyPhone}></li>
+          </ul>
+          <ul className={css.clergyItem}>
+            <li className={css.clergyName}>Диякон Андрій Прокопенко</li>
+            <li className={css.clergyPhone}></li>
+          </ul>
         </ul>
       </article>
     </div>
