9d74a400a73650fdeab52c42adcc8e95217b3b49
Valeriy Svystun <valeriy.all@gmail.com>
2025-12-01 12:35:18 +0200

    renew(schedule.json): december

--- src/app/shedule/page.tsx
+++ src/app/shedule/page.tsx
@@ -31,7 +31,7 @@ export default function ShedulePage() {
 
   return (
     <section className={`container ${styles.wrapper}`}>
-      <h1 className={styles.title}>Розклад Богослужінь — Листопад</h1>
+      <h1 className={styles.title}>Розклад Богослужінь — Грудень</h1>
 
       <div className={styles.tableWrapper}>
         <table className={styles.table}>
