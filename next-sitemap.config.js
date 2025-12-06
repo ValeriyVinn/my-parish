8a1281954d2dca8268c8f5086faffc644e579c0f
Valeriy Svystun <valeriy.all@gmail.com>
2025-12-06 23:41:33 +0200

    updated(next-sitemap.config.js): robotsTxtOptions

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,4 +3,16 @@ module.exports = {
   siteUrl: 'https://parish-ten.vercel.app',
   generateRobotsTxt: true,
   sitemapSize: 7000,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        disallow: ['/aid', '/calendar', '/gallery'],
+      },
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+  },
 };
