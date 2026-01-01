dba9f74ca9d9c08d30ebc7279d38154216748ce4
Valeriy Svystun <valeriy.all@gmail.com>
2026-01-01 23:25:26 +0200

    added(lives): seraphim-sarovskyi page

--- src/app/events/lives/seraphim-sarovskyi/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function AscensionPage() {
-  return (
-    <div style={{ padding: "2rem", textAlign: "center" }}>
-      <h1>Свт. Серафім Саровський</h1>
-      <p>Сторінка в розробці</p>
-    </div>
-  );
-}
